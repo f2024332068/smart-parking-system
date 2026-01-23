b858ebfb566f74e10d752b97496bbf78df7665e2
f2024332068 <f2024332068@umt.edu.pk>
2026-01-23 16:57:43 +0500

    Create rollbackmanager.cpp
    This class maintains a linked-list history of vehicle actions to support rollback tracking and display.

---
+++ rollbackmanager.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+#include"RollbackManager.h"
+using namespace std;
+RollbackManager::RollbackManager(){
+head = NULL;
+}
+void RollbackManager :: log(int v, int z, const char* act )
+{
+HistoryNode* n = new HistoryNode;
+n->vehicleID = v;
+n->zoneID = z;
+strcpy(n->action , act);
+n->next = head;
+head = n;
+}
+void RollbackManager::showHistory(){
+if(!head){
+cout<<"No History available\n";
+return;
+}
+HistoryNode* t = head;
+cout<<"\n--- HISTORY---\n";
+while(t){
+cout<<"Vehicle" << t->vehicleID
+<< "| Zone " << t->zoneID
+<< "| " <<t->action<< endl;
+t =t->next;
+}
+}
